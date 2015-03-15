a6bdb4100b302d354d5dcc844f6b8f7a002b06f0
adrianwestglenn@yahoo.com <adrianwestglenn@yahoo.com>
2015-03-15 14:10:59 -0500

    Test commit 1

---
+++ js/common.js
@@ -0,0 +1,19 @@
+			jQuery(document).ready(function($){
+
+				var edgar = "#ff22ff";
+
+				var text = 'Chocolat3Bliss';
+
+				$( 'h1').animate({
+   				 	opacity: 0.15,
+   				 	color: "red",
+   				 	fontSize: "4px",
+   				 	left: "+=500",
+   				 }, 3000);
+
+				$("body").css("background",edgar);
+
+				$("h1").text("Chocolat3Bliss");
+
+			});
+		
---
+++ js/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+
+	function launch_confirm(){
+		var msg = window.prompt("Write your name", "Name");
+		jQuery('.site-title').text(msg).promise().done(function(){
+			jQuery('.site-title').animate({
+				"color":"#aaa",
+				"font-size": "32px"
+			}, 1000);
+		})
+	}
+
+	setTimeout(function(){
+		launch_confirm();
+	},1000);
+
+});
+//test
+
